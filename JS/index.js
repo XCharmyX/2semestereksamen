6964e33b9ecaec6e5aea6d2c5743ff7f2ab3d3d0
XCharmyX <60596287+XCharmyX@users.noreply.github.com>
2020-05-19 16:45:20 +0200

    omkodning af javascript

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,7 @@
-<script>
-$('.box_box').hide();
-$('.box_heading').click(function(){
-$(this).next('.box_box').fadeToggle('slow');
-$(this).find('a').toggleClass('active');
-$(this).toggleClass('change');
-});
-</script>
+console.log("it works");
+document.getElementById("kundebtn").onclick = function() {slideInKunde()};
+function slideInKunde() {
+  var win = document.getElementByClass("kundeslide");
+  win.style.transition = "right 1s ease-in-out 0s";
+	win.style.left = "0px";
+}

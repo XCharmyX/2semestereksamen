bc88289aa353f9cd5449ef1207cbd7838526feaf
XCharmyX <60596287+XCharmyX@users.noreply.github.com>
2020-05-19 15:24:44 +0200

    opdatering af javascript

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,8 @@
-.
+<script>
+$('.box_box').hide();
+$('.box_heading').click(function(){
+$(this).next('.box_box').fadeToggle('slow');
+$(this).find('a').toggleClass('active');
+$(this).toggleClass('change');
+});
+</script>

d141ea550ed74f720d3fa3457b89e2f2877c9b24
XCharmyX <60596287+XCharmyX@users.noreply.github.com>
2020-05-23 19:10:50 +0200

    fixed nav og collapsible

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,23 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    if (content.style.display === "block") {
+      content.style.display = "none";
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.display = "block";
     }
   });
 }
